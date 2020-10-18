259d64be01d3e2be5cb6889c9cec003450fb38b5
mugi <m.111_t.1230_nittc@outlook.com>
2020-10-18 15:29:26 +0900

    get contests list

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import * as core from '@actions/core'
 import scraping from './scraping'
 
 async function run(): Promise<void> {
+  let contents = new Array<string | undefined>();
   try {
-    scraping('https://atcoder.jp/contests/')
+    contents = await scraping('https://atcoder.jp/contests/')
   } catch (error) {
     core.setFailed(error.message)
   }
--- src/scraping.ts
+++ src/scraping.ts
@@ -1,8 +1,20 @@
 import superagent from 'superagent'
+import cheerio from 'cheerio'
 
-const scraping = async (url: string): Promise<void> => {
+const scraping = async (url: string): Promise<Array<string | undefined>> => {
   const result = await superagent.get(url)
-  console.log(result.text)
+  const contents = new Array<string | undefined>();
+  const $ = cheerio.load(result.text)
+  $('div#contest-table-upcoming').find('tr > td > a').each((i, e) => {
+    e.children.forEach((e) => {
+      if (e.type == 'text') {
+        contents.push(e.data)
+      } else {
+        contents.push(e.children[0].data)
+      }
+    })
+  })
+  return contents
 }
 
 export default scraping
